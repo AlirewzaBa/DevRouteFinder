c1f4ef2571845ce615030873dc15959505f7357b
alireza <150370723+AlirewzaBa@users.noreply.github.com>
2023-12-23 02:12:14 +0330

    Update styles.css

--- css/styles.css
+++ css/styles.css
@@ -1,1 +1,20 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f0f0f0;
+}
+
+header {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 20px 0;
+}
+
+main {
+  width: 80%;
+  margin: 20px auto;
+  text-align: center;
+}
 
