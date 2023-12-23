56d4f69132fe0efba7a3f5f7ee41f8b7c2b9a698
alireza <150370723+AlirewzaBa@users.noreply.github.com>
2023-12-23 18:43:09 +0330

    Feature/new feature (#1)
    * Create new-feature.js
    
    * Create feature-section.html
    
    * Create feature-section.css

---
+++ new-feature.js
@@ -0,0 +1,6 @@
+document.getElementById('toggleBtn').addEventListener('click', function() {
+  var menu = document.getElementById('menu');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style
