79e419865cac03fc7cb6a8e4eada0682fca23c27
Hossein <147403540+ho3einrp@users.noreply.github.com>
2023-12-24 01:13:25 +0330

    Create testing.js

---
+++ testing.js
@@ -0,0 +1,8 @@
+<ul>
+    <li><a target="_blank" href="https://youtu.be/SpbRY1IRUko" data-text="Home">Home</a></li>
+    <li><a target="_blank" href="https://youtu.be/SpbRY1IRUko" data-text="Blogs">Blogs</a></li>
+    <li><a target="_blank" href="https://youtu.be/SpbRY1IRUko" data-text="Coding">Coding</a></li>
+    <li><a target="_blank" href="https://youtu.be/SpbRY1IRUko" data-text="Skills">Skills</a></li>
+    <li><a target="_blank" href="https://youtu.be/SpbRY1IRUko" data-text="Extras">Extras</a></li>
+    <li><a target="_blank" href="https://youtu.be/SpbRY1IRUko" data-text="Contact">Contact</a></li>
+  </ul>
